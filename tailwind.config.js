1a5892895b0e7c2522f68626b62de41ffd5e827e
Firstname Lastname <carlguilford@gmail.com>
2024-12-02 19:01:38 +1300

    inital again

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export const content = [
-  "./src/app/**/*.{js,ts,jsx,tsx}",
- 
-];
-export const theme = {
-  extend: {},
-};
-export const plugins = [];
